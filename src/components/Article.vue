a69fa0e1d75b0f7f1434343977c59945b453bc01
lu <jetlu0089@126.com>
2019-10-01 17:31:38 +0800

    修改文件

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,38 +1,46 @@
 <template>
-<div>
-<yd-cell-group slot="list" class="article">
-  <yd-cell-item arrow v-for="(item,index) in articleList" :key="index" @click.native="detail(item.id)">
-    <div slot="left"><span>{{ item.title }}</span> <yd-badge shape="square" class="time" type="warning">{{item.display_time}}</yd-badge></div>
-    <span slot="right">{{ item.author }}</span>
-    </yd-cell-item>
-    <yd-cell-item v-if="articleList.length===0">
-      <span slot="left">暂时没有数据</span>
-    </yd-cell-item>
-</yd-cell-group>
-
- <div class="roundplus" @click="addArticle()">+</div>
- <div class="popup-module">
-        <yd-popup v-model="showDetail" position="bottom" height="60%">
-            <div slot="top" class="popup-title">信息</div>
-            <div>
-                <yd-cell-group>
-            <yd-cell-item>
-                <span slot="left">标题</span>
-                <span slot="right">{{articleInfo.title}}</span>
-            </yd-cell-item>
-            <yd-cell-item>
-                <span slot="left">作者</span>
-                <span slot="right">{{articleInfo.author}}</span>
-            </yd-cell-item>
-              <yd-cell-item>
-                <span slot="left">时间</span>
-                <span slot="right">{{articleInfo.display_time}}</span>
-            </yd-cell-item>
-        </yd-cell-group>
-            </div>
-        </yd-popup>
+  <div>
+    <yd-cell-group slot="list" class="article">
+      <yd-cell-item
+        arrow
+        v-for="(item,index) in articleList"
+        :key="index"
+        @click.native="detail(item.id)"
+      >
+        <div slot="left">
+          <span>{{ item.title }}</span>
+          <yd-badge shape="square" class="time" type="warning">{{item.display_time}}</yd-badge>
         </div>
-</div>
+        <span slot="right">{{ item.author }}</span>
+      </yd-cell-item>
+      <yd-cell-item v-if="articleList.length===0">
+        <span slot="left">暂时没有数据</span>
+      </yd-cell-item>
+    </yd-cell-group>
+
+    <div class="roundplus" @click="addArticle()">+</div>
+    <div class="popup-module">
+      <yd-popup v-model="showDetail" position="bottom" height="60%">
+        <div slot="top" class="popup-title">信息</div>
+        <div>
+          <yd-cell-group>
+            <yd-cell-item>
+              <span slot="left">标题</span>
+              <span slot="right">{{articleInfo.title}}</span>
+            </yd-cell-item>
+            <yd-cell-item>
+              <span slot="left">作者</span>
+              <span slot="right">{{articleInfo.author}}</span>
+            </yd-cell-item>
+            <yd-cell-item>
+              <span slot="left">时间</span>
+              <span slot="right">{{articleInfo.display_time}}</span>
+            </yd-cell-item>
+          </yd-cell-group>
+        </div>
+      </yd-popup>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -117,6 +125,6 @@ export default {
   height: 0.6rem;
   line-height: 0.6rem;
   background-color: #e8e8e8;
-  transition-timing-function: cubic-bezier(0.785, 0.135, 0.15, 0.86)
+  transition-timing-function: cubic-bezier(0.785, 0.135, 0.15, 0.86);
 }
 </style>
